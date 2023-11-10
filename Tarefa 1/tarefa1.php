e02793c83f7714ebf561c562c9c0c151f63a9fad
juliazimmermann2023 <128699018+juliazimmermann2023@users.noreply.github.com>
2023-11-10 14:50:16 -0300

    Update tarefa1.php

--- Tarefa 1/tarefa1.php
+++ Tarefa 1/tarefa1.php
@@ -8,11 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body {
-            background: rgb(255,190,241);
-background: radial-gradient(circle, rgba(255,190,241,1) 0%, rgba(240,90,214,1) 100%);
+            background: rgb(255, 190, 241);
+            background: radial-gradient(circle, rgba(255, 190, 241, 1) 0%, rgba(240, 90, 214, 1) 100%);
             height: 100vh;
         }
 
+        @import url('https://fonts.googleapis.com/css2?family=Agbalumo&display=swap');
+        
+  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Roboto+Mono:ital,wght@1,300&display=swap');
 
         body {
             text-align: center;
@@ -24,39 +27,63 @@ background: radial-gradient(circle, rgba(255,190,241,1) 0%, rgba(240,90,214,1) 1
             padding: 25px;
             border: solid 1px;
         }
-        img{ 
+
+        img {
             width: 1%;
         }
-
+h2{
+   font-family: 'Roboto+Mono';
+ 
+            text-align: center;
+            font-size: 3em;
+         
+}
+        h1 {
+            font-family: 'Agbalumo', serif;
+            text-align: center;
+            font-size: 3em;
+            background-color: rgba(245, 123, 214, 0.952);
+            margin-bottom: 30px;
+        }
     </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Roboto+Mono:ital,wght@1,300&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
-   
-<?php
-function isPrimo($posicao) {
-    $primos= array(2);
-    $num = 3;
-    while(count($primos) < $posicao){
-        $v_primo = true;
-    foreach($primos as $primo){
-        if($num%$primo == 0){
-            $v_primo = false;
+
+    <?php
+    function isPrimo($posicao)
+    {
+        $primos = array(2);
+        $num = 3;
+        while (count($primos) < $posicao) {
+            $v_primo = true;
+            foreach ($primos as $primo) {
+                if ($num % $primo == 0) {
+                    $v_primo = false;
+                }
+            }
+            if ($v_primo) {
+                $primos[] = $num;
+            }
+            $num++;
         }
+        return $primos[$posicao - 1];
     }
-    if($v_primo){
-        $primos[] = $num;
-    }
-    $num++;
-        }
-        return$primos[$posicao - 1];
-    }
-   
-
-?>
 
 
-    <h1> Calculadora Primos</h1>
+
+    ?>
+
+
+    <h1> Calculadora de Números Primos</h1>
     <div class="row">
         <div class="col-md-6 offset-md-3">
 
@@ -77,23 +104,23 @@ function isPrimo($posicao) {
                 <div class="mb-3">
                     <button type="submit" class="btn btn-success">Enviar</button>
                 </div>
- 
-                <?php              
-if (isset($_POST["posicao"])) {
-?>
- 
-                <h2> Resultado </h2>
-                <div class="row text-center">
-                    <div class="col-3">
-                        <small>Valor:</small>
-                        <h3><?php echo isPrimo($_POST["posicao"]); ?> </h3>
-                    </div>
 
-                        
-               
                 <?php
-                }
+                if (isset($_POST["posicao"])) {
                 ?>
+
+                    <h2> Resultado: </h2>
+                    <div class="row text-center">
+                        <div class="col-3">
+                            <small>Valor:</small>
+                            <h3><?php echo isPrimo($_POST["posicao"]); ?> </h3>
+                        </div>
+
+
+
+                    <?php
+                }
+                    ?>
 
 
             </form>
